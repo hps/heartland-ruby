818e8f2af0cb257da5adb0693b940c82f8e537d7
asmockler <asmockler@gmail.com>
2015-01-05 14:35:22 -0700

    Whitespace

--- lib/hps/entities/hps_cardholder.rb
+++ lib/hps/entities/hps_cardholder.rb
@@ -1,11 +1,11 @@
 module Hps
-	class HpsCardHolder
+  class HpsCardHolder
 
-		attr_accessor :first_name, :last_name, :phone, :email_address, :address
+    attr_accessor :first_name, :last_name, :phone, :email_address, :address
 
-		def initialize
-			self.address = Hps::HpsAddress.new
-		end
-
+    def initialize
+	  self.address = Hps::HpsAddress.new
 	end
+
+  end
 end
